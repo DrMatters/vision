d5de3088959fc065f6ba97dae44898c3fbd2dcda
DrMatters <qseminq@gmail.com>
2019-11-25 18:27:07 +0300

    Stash

---
+++ simple/background.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+
+
+def main():
+    cap = cv2.VideoCapture('../data/videos/test.mp4')
+    mog = cv2.createBackgroundSubtractorMOG2()
+    knn = cv2.createBackgroundSubtractorKNN()
+
+    codec = cv2.VideoWriter_fourcc(*'XVID')
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    writer = cv2.VideoWriter('../data/videos/output/test.avi', codec,
+                             fps, (216, 1152),
+                             isColor=False)
+
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret is False:
+            break
+        frame = cv2.resize(frame, None, fx=0.3, fy=0.3)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        mog_mask = mog.apply(frame)
+        knn_mask = knn.apply(frame)
+
+        frame = np.concatenate((frame, mog_mask, knn_mask), 1)
+
+        writer.write(frame)
+        cv2.imshow('vid', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
