2f5ed0b7594b0b13a0a367cff21a3f3deaea7b37
DrMatters <qseminq@gmail.com>
2019-11-27 16:01:37 +0300

    Add code for ORB calculation

---
+++ features_task/orb.py
@@ -0,0 +1,15 @@
+import operator
+
+import cv2
+
+import utils
+
+img = cv2.imread('../data/images/human/3.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+orb: cv2.ORB = cv2.ORB_create()
+keypoints = orb.detect(img, None)
+
+keypoints = sorted(keypoints, key=operator.attrgetter('response'), reverse=True)
+
+utils.draw_percent_steps(keypoints, img, 10)
