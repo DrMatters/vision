5baf75e340bc400a72526df345370904106210d2
DrMatters <qseminq@gmail.com>
2019-11-28 19:17:49 +0300

    Resolvers now should return descriptors
     * (for example if you want to count mean)

--- features_task/real/non_ml_features.py
+++ features_task/real/non_ml_features.py
@@ -1,5 +1,5 @@
 import collections
-from typing import Callable, Tuple, List, Set
+from typing import Callable, Tuple, List
 
 import cv2
 import numpy as np
@@ -8,30 +8,31 @@ import numpy as np
 class DownsamplingFill:
     def __init__(self, cell_side_len: int,
                  detect_and_compute: Callable[[np.ndarray], Tuple[List[cv2.KeyPoint], np.ndarray]],
-                 resolve_multiple: Callable[[Set[cv2.KeyPoint]], cv2.KeyPoint]):
+                 resolve_multiple: Callable[[List[cv2.KeyPoint], List[np.ndarray]], np.ndarray]):
         self._cell_side = cell_side_len
         self._extract = detect_and_compute
         self._resolve = resolve_multiple
 
     def downsample(self, image: np.array):
         keypoints, descriptors = self._extract(image)
-        cells_index_def = collections.defaultdict(set)
+        keypoints_of_cell = collections.defaultdict(list)
+        descriptors_of_cell = collections.defaultdict(list)
         keypoints_index = {}
         for index, keypoint in enumerate(keypoints):
             # save mapping from cv2.KeyPoint to it's index to find descriptor later
             keypoints_index[keypoint.pt + (keypoint.angle,)] = index
             cell_x = int(keypoint.pt[0] // self._cell_side)
             cell_y = int(keypoint.pt[1] // self._cell_side)
-            cells_index_def[(cell_x, cell_y)].add(keypoint)
+            keypoints_of_cell[(cell_x, cell_y)].append(keypoint)
+            descriptors_of_cell[(cell_x, cell_y)].append(descriptors[index])
 
         grid_shape = np.asarray(image.shape) // self._cell_side
         # add one dimension for descriptor vector
         grid_shape = np.append(grid_shape, descriptors.shape[1])
         grid = np.zeros(grid_shape, dtype=np.uint8)
 
-        for cell, cell_keypoints in cells_index_def.items():
-            keypoint = self._resolve(cell_keypoints)
-            index = keypoints_index[keypoint.pt + (keypoint.angle,)]
-            descriptor = descriptors[index]
+        for cell, cell_keypoints in keypoints_of_cell.items():
+            cell_descriptors = descriptors_of_cell[cell]
+            descriptor = self._resolve(cell_keypoints, cell_descriptors)
             grid[cell[1], cell[0]] = descriptor
         return grid
--- features_task/test_downsample.py
+++ features_task/test_downsample.py
@@ -1,25 +1,39 @@
 import functools
-from typing import Set
+from typing import List
 
 import cv2
+import numpy as np
 
 from features_task.real import non_ml_features
 
 
-def resolve_max_response(kps: Set[cv2.KeyPoint]) -> cv2.KeyPoint:
-    return max(kps, key=lambda item: item.response)
+def resolve_max_response(keypoints: List[cv2.KeyPoint],
+                         descriptors: List[np.ndarray]) -> np.ndarray:
+    index = range(len(keypoints))
+    max_index = max(index, key=lambda item: keypoints[item].response)
+    return descriptors[max_index]
+
+
+def resolve_first(keypoints: List[cv2.KeyPoint],
+                  descriptors: List[np.ndarray]) -> np.ndarray:
+    return descriptors[0]
 
 
 orb: cv2.ORB = cv2.ORB_create()
 detect_and_compute = functools.partial(orb.detectAndCompute, mask=None)
 
-orb_downsampler = non_ml_features.DownsamplingFill(
+max_resp_downs = non_ml_features.DownsamplingFill(
     8, detect_and_compute, resolve_max_response
+)
+
+first_downs = non_ml_features.DownsamplingFill(
+    8, detect_and_compute, resolve_first
 )
 
 img = cv2.imread('../data/images/human/3.png')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-res = orb_downsampler.downsample(gray)
+res_max = max_resp_downs.downsample(gray)
+res_first = first_downs.downsample(gray)
 
-print(res)
+print(res_max)
