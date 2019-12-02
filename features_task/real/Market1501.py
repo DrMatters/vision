7ddbc16c39974f5b099d39f6e5c2c0ead8d9ce63
DrMatters <qseminq@gmail.com>
2019-12-02 14:30:14 +0300

    Finish dataset for Market1501

--- features_task/real/Market1501.py
+++ features_task/real/Market1501.py
@@ -1,17 +1,40 @@
+import os
+from typing import FrozenSet
+
 import albumentations as albu
 import pandas as pd
 from torch.utils import data
 
 
 class MarketDataset(data.Dataset):
-    def __init__(self, folder: str, index_df: pd.DataFrame = None,
-                 transforms=albu.Compose([albu.HorizontalFlip()])):
-        self.folder = folder
-        self.index_df = index_df
-        self.transforms = transforms
+    REQUIRED_COLUMNS: FrozenSet[str] = frozenset({'filename', 'pers_id'})
 
-    def __getitem__(self, item):
-        pass
+    def __init__(self, folder: str, index_df: pd.DataFrame = None,
+                 transforms=albu.Compose([albu.HorizontalFlip()]),
+                 preprocessing=None):
+        assert set(index_df.columns).issubset(self.REQUIRED_COLUMNS), \
+            ('Required columns are not present in dataframe. Expected:'
+             f' {self.REQUIRED_COLUMNS}. Got: {set(index_df.columns)}')
+        self._folder = folder
+        self._index_df = index_df
+        self._transforms = transforms
+        self._preprocessing = preprocessing
+
+    def __getitem__(self, idx):
+        row = self._index_df.loc[idx, :]
+        filename = row['filename']
+        person_id = row['pers_id']
+
+        full_path = os.path.join(self._folder, filename)
+
+        img = cv2.imread(full_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        augmented = self._transforms(image=img)
+        img = augmented['image']
+        if self._preprocessing:
+            preprocessed = self._preprocessing(image=img)
+            img = preprocessed['image']
+        return img, person_id
 
     def __len__(self):
-        return len(self.index_df)
+        return len(self._index_df)
